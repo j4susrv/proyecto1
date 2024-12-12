c61b5a727bc38bc671eece44247256e0bf28cb95
j4susrv <jesus.emanuel.rivera.rivera@gmail.com>
2024-12-12 11:55:51 -0300

    Descripción de los cambios

--- controlers.py
+++ controlers.py
@@ -2,14 +2,13 @@ from models import Administrador
 from forms import FormularioRegistroLoginAdministrador  
 from app import db
 from werkzeug.security import generate_password_hash
-
+#Controlador de los Administradores
 class ControladorAdministrador():
     def crear_usuario(self, correo, contraseña, confirmar_contraseña):
         if contraseña != confirmar_contraseña:
             return "Las contraseñas no coinciden"
 
         # Hashea la contraseña antes de guardarla en la base de datos
-        # Utiliza un algoritmo de hashing seguro como bcrypt o Argon2
         hashed_password = generate_password_hash(contraseña)
 
         nuevo_admin = Administrador(correo=correo, contraseña=hashed_password)
